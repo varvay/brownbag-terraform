78de55819b32eafe92aa9e810e1d4e80635c9709
varvay <varidvaya@outlook.com>
2022-11-28 18:25:54 +0700

    enrich variables description

--- modules/database/variables.tf
+++ modules/database/variables.tf
@@ -2,14 +2,17 @@ variable "environment" {
   type = string
   default = "dev"
   nullable = false
+  description = "Terraform application destination e.g. dev, staging, prod"
 }
 
 variable "subnet_id" {
   type = string
   nullable = false
+  description = "Backend server AWS deployment destination subnet's IDs"
 }
 
 variable "security_group_id" {
   type = string
   nullable = false
+  description = "Backend server AWS security group's ID"
 }
